86289830b8818235243ab44bb20296849982d81a
MJ <mikey.j.1228@gmail.com>
2022-06-14 22:54:45 +0900

    Solve solution albeit complicated than necessary

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    /* split string input by space and place in an array*/
+    const words = s.split(' ');
+    let wordHolder = '';
+    let reverseList = [];
+    /* for all array values perform a reversal and append to reverseList*/
+    for (word of words) {
+        for (let i = word.length-1; i >= 0; i--) {
+            wordHolder += word[i];
+        }
+        reverseList.push(wordHolder);
+        wordHolder = '';
+    } 
+    reverseList = reverseList.reverse();
+    return reverseList.join(' ');
 };
 
 // Do not edit below this line
