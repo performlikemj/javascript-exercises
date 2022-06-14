6b8ba2ab2421029e877fccb8b6b0f5e3f5b867f5
MJ <mikey.j.1228@gmail.com>
2022-06-14 22:55:37 +0900

    Complete all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    const inputString = s;
+    let output = ''
+    if (n < 0) {
+        return "ERROR";
+    }
+    while (n > 0) {
+        output += `${inputString}`;
+        n--; 
+    } 
+    return output;
 };
 
 // Do not edit below this line
