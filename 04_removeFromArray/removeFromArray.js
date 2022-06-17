18165309af99614f2fb8e7be245637857f40d644
MJ <mikey.j.1228@gmail.com>
2022-06-17 11:01:39 +0900

    Complete all tests from spec

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (let i = 0; i < array.length; i++) {
+        for (value of valuesToRemove) {
+            if (array[i] === value) {
+                array.splice(i, 1);
+                removeFromArray(array, ...valuesToRemove);
+            } 
+        }
+    }
 
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
