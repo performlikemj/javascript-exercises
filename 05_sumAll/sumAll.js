51915bd4fdc6ab3aabe2b92f297bfa0fa40a4a11
MJ <mikey.j.1228@gmail.com>
2022-06-18 15:18:16 +0900

    Complete all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let sum = 0;
+    let min;
+    let max;
+    if (args[0] < args[1]) {
+        min = args[0];
+        max = args[1];
+    } else {
+        max = args[0];
+        min = args[1];
+    }
 
+    if (min < 0 || max < 0 || typeof(min) != "number" || typeof(max) != "number") {
+        return "ERROR"
+    } else {
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }   
+    }
+
+    return sum;
+    
 };
 
 // Do not edit below this line
