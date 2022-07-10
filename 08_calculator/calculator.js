bbf4bbeffadef0c4b9bf4bffd9649d1f9f6d0167
MJ <mikey.j.1228@gmail.com>
2022-07-10 22:51:59 +0900

    Complete all tests with their appropriate functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...values) {
+	const add = values.reduce((total, value) => total += value, 0);
+  return add;
 };
 
-const subtract = function() {
-	
+const subtract = function(...values) {
+	const minus = values.reduce((sub, value) => sub -= value);
+  return minus;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  const sum = values.reduce((total, value) => total += value, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  const multi = values.reduce((total, value) => total *= value);
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	const pow = a**b;
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	if (value === 0) {
+    return 1;
+  }
+  let numberList = [];
+  for (let i = 1; i <= value; i++){
+    numberList.push(i);
+  }
+  const fact = numberList.reduce((total, number) => total *= number);
+  return fact;
 };
 
 // Do not edit below this line
