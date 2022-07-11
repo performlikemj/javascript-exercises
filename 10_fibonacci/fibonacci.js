cb69e779a4a0fb1d2a9a31d5b8c78174c0c52767
MJ <mikey.j.1228@gmail.com>
2022-07-11 23:05:30 +0900

    Solve all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let placeholder = [1];
+    let a = 1;
+    let b = 1; 
+    if (Number(number) < 1 ) {
+        return 'OOPS'
+    }
+    while (placeholder.length != Number(number)+1) {
+        c = a + b;
+        a = b;
+        b = c;
+        placeholder.push(a);
+    }
 
+    return placeholder[Number(number)-1];
 };
 
 // Do not edit below this line
