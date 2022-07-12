62232157059d47fa6682954aed903c458dabf270
MJ <mikey.j.1228@gmail.com>
2022-07-12 22:52:24 +0900

    Solve test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+  let books = [];
+  for (let x = 0 ; x < obj.length ; x++)
+    for (const key in obj[x]) {
+        if (key === 'title') {
+            books.push(obj[x][key])
+        }
+    }  
+  return books;
 };
 
 // Do not edit below this line
