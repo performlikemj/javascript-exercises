62c6cda66630df473753a2c50ea8f19e71115da0
MJ <mikey.j.1228@gmail.com>
2022-07-15 13:10:09 +0900

    Solve all tests

--- 12_findTheOldest/README.md
+++ 12_findTheOldest/README.md
@@ -2,6 +2,8 @@
 
 Given an array of objects representing people with a birth and death year, return the oldest person.
 
+Create a list outside the loop, within the loop create objects with the individual people and their updated lifespan. Could have used map() for first part of function
+
 ## Hints
 - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
 - this can be done with a couple of chained array methods, or by using `reduce`.
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let people = [];
+    for (let x = 0; x < obj.length; x++) {
+        let dataObject = {};
+        dataObject['name'] = obj[x]['name'];
+        if (obj[x]['yearOfDeath']) {
+            dataObject['lifespan'] =  obj[x]['yearOfDeath'] - obj[x]['yearOfBirth'];
+        } else {
+            dataObject['lifespan'] = Number(new Date().getFullYear() - obj[x]['yearOfBirth']);
+ 
+        }  
+        people.push(dataObject);
+    }
+    const oldest = people.sort((personA, personB) => personB['lifespan'] - personA['lifespan']);
+    
+    return oldest[0];
 
 };
 
